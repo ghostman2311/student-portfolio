431346f2c169410b14f92a9e5f9873052af35ca0
Nikhil <nkarnwal07@gmail.com>
2024-04-18 01:25:53 +0530

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,14 @@ import "./App.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Hangman from "./pages/hangman/Hangman";
+import step0 from "./assets/stepImages/0.jpg";
+import step1 from "./assets/stepImages/1.jpg";
+import step2 from "./assets/stepImages/2.jpg";
+import step3 from "./assets/stepImages/3.jpg";
+import step4 from "./assets/stepImages/4.jpg";
+import step5 from "./assets/stepImages/5.jpg";
+import step6 from "./assets/stepImages/6.jpg";
 
 function App() {
   return (
@@ -13,9 +21,25 @@ function App() {
         <Header />
         <main className="content">
           <Routes>
-            <Route path="/" element={<Project/>}></Route>
+            <Route
+              path="/"
+              element={
+                <Hangman
+                  maxWrong={6}
+                  images={[step0, step1, step2, step3, step4, step5, step6]}
+                />
+              }
+            ></Route>
             <Route path="/project" element={<Project />}></Route>
-            <Route path="/about" element={<About />}></Route>
+            <Route
+              path="/about"
+              element={
+                <>
+                  <About />
+                  <Join />
+                </>
+              }
+            ></Route>
           </Routes>
         </main>
         <Footer />
